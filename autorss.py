3de45a9dd8ad398508a2ca9b5464c287742ac8b9
Jakub Starostecki <jakub.starostecki27@gmail.com>
2023-01-30 21:50:30 +0100

    RSS is now compliant with validator.w3.org
    https://validator.w3.org/feed/check.cgi?url=https%3A%2F%2Fblog.pouekdev.one%2FRSS.xml

--- autorss.py
+++ autorss.py
@@ -1,11 +1,11 @@
 import sys
 import os
-from datetime import date
+import datetime
 import xml.etree.ElementTree as ET
 post_file = ""
 rss_file = "rsstest.xml"
 lines = []
-today = date.today()
+today = datetime.datetime.now()
 post_number = str(sys.argv[1])
 for file in os.listdir("posts/"):
     if file.startswith(post_number):
@@ -19,14 +19,15 @@ else:
             lines.append(line)
     tree = ET.parse(rss_file)
     root = tree.getroot()
-    i = 0
+    root = root[0]
     item = ET.Element("item")
     title = ET.Element("title")
     title.text = lines[1].replace("#TITL","")
     link = ET.Element("link")
-    link.text = "?post="+str(post_file)
+    link.text = "https://blog.pouekdev.one/?post="+str(post_file)
     guid = ET.Element("guid")
     guid.text = "?post="+str(post_file)
+    guid.set("isPermaLink","false")
     day_name = today.strftime("%A")
     day_name = day_name[0:3]
     day_name += ","
@@ -34,8 +35,9 @@ else:
     month = today.strftime("%B")
     month = month[0:3]
     year = today.strftime("%Y")
+    other = today.strftime("%H:%M:%S")
     pdate = ET.Element("pubDate")
-    pdate.text = day_name + " " + day_number + " " + month + " " + year
+    pdate.text = day_name + " " + day_number + " " + month + " " + year + " " + other + " +0100"
     for line in lines:
         try:
             line.index("#TEXT")
@@ -45,6 +47,8 @@ else:
             description_text = line
             break
     description_text = description_text.replace("#TEXT","")
+    description_text = description_text[:-1]
+    description_text += " [...]"
     description = ET.Element("description")
     description.text = description_text
     item.insert(0,title)
@@ -52,5 +56,5 @@ else:
     item.insert(2,guid)
     item.insert(3,pdate)
     item.insert(4,description)
-    root.insert(1,item)
+    root.insert(4,item)
     tree.write(rss_file, "UTF-8")
